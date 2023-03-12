f25968a9d7a24cfe65638eca6915f84f7aa8e0f5
Shubham5250 <shubhampaw312@gmail.com>
2023-03-12 23:11:24 +0530

    Layout done

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,3 @@
-
 /// It contains Flutter widgets implementing Material Design.
 /// Material is a design system created by Google to help teams build high-quality digital experiences for Android, iOS, Flutter, and the web.
 import 'package:flutter/material.dart';
@@ -9,17 +8,16 @@ void main() {
 }
 
 //FLUTTER WIDGETS =>
-  // 1. Stateless widgets:  Stateless widgets are the widgets that don’t change i.e. they are immutable.
-        // To create a Stateless widget we have to override build() method
-        // Example : Icon, IconButton and Text....
+// 1. Stateless widgets:  Stateless widgets are the widgets that don’t change i.e. they are immutable.
+// To create a Stateless widget we have to override build() method
+// Example : Icon, IconButton and Text....
 
-  // 2. Stateful widgets:  Stateful Widgets are the ones that change its properties during run-time.
-        // They are dynamic i.e., they are mutable and can be drawn multiple times within its lifetime.
-        // It can change its appearance in response to events triggered by user interactions or when it receives data.
-        // To create a Stateful widget we need to override a createState() method
-        // Example : Checkbox, Radio Button, Slider, InkWell, Form, and TextField
+// 2. Stateful widgets:  Stateful Widgets are the ones that change its properties during run-time.
+// They are dynamic i.e., they are mutable and can be drawn multiple times within its lifetime.
+// It can change its appearance in response to events triggered by user interactions or when it receives data.
+// To create a Stateful widget we need to override a createState() method
+// Example : Checkbox, Radio Button, Slider, InkWell, Form, and TextField
 class MyApp extends StatelessWidget {
-
   // This is an constructor of class 'MyHomePage'
   // In Flutter, keys store the widget’s state when the device moves from one branch to another in the widget tree.
   // Keys can be found in almost every widget as named parameters. It is useful when we need to store the widget’s state having the same type of data.
@@ -33,20 +31,127 @@ class MyApp extends StatelessWidget {
   // The BuildContext of each widget is passed to their build method. Remember that the build method returns the widget tree a widget renders.
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+      theme: ThemeData(),
+      home: const Calculator(),
+    );
+  }
+}
+
+class Calculator extends StatefulWidget {
+  const Calculator({Key? key}) : super(key: key);
+
+  @override
+  State<Calculator> createState() => _CalculatorState();
+}
+
+class _CalculatorState extends State<Calculator> {
+  Widget calcButton(String btnText, Color btnColor, Color textColor) {
+    return Container(
+      child: ElevatedButton(
+        onPressed: (() {}),
+        child: Text(
+          btnText,
+          style: TextStyle(fontSize: 35, color: textColor),
+        ),
+        style: ElevatedButton.styleFrom(
+            backgroundColor: btnColor,
+            fixedSize: const Size(80, 80),
+            shape: CircleBorder(),
+            padding: EdgeInsets.all(10)),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black54,
+      appBar: AppBar(
+        title: const Text("Calculator"),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 5),
+        child: Column(
+          // This will put the textfields to bottom & will start building with bottom up approach
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: const [
+                Padding(
+                  padding: EdgeInsets.all(10),
+                  child: Text(
+                    "0",
+                    textAlign: TextAlign.left,
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 50,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(height: 10,),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                // These are the functioning buttons
+                calcButton('AC', Colors.grey, Colors.black),
+                calcButton('+/-', Colors.grey, Colors.black),
+                calcButton('%', Colors.grey, Colors.black),
+                calcButton('÷', Colors.orangeAccent, Colors.white),
+              ],
+            ),
+            SizedBox(height: 10,),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                // These are the functioning buttons
+                calcButton('9', Colors.grey.shade800, Colors.white),
+                calcButton('8', Colors.grey.shade800, Colors.white),
+                calcButton('7', Colors.grey.shade800, Colors.white),
+                calcButton('x', Colors.orangeAccent, Colors.white),
+              ],
+            ),
+            SizedBox(height: 10,),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                // These are the functioning buttons
+                calcButton('6', Colors.grey.shade800, Colors.white),
+                calcButton('5', Colors.grey.shade800, Colors.white),
+                calcButton('4', Colors.grey.shade800, Colors.white),
+                calcButton('-', Colors.orangeAccent, Colors.white),
+              ],
+            ),
+            SizedBox(height: 10,),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                // These are the functioning buttons
+                calcButton('3', Colors.grey.shade800, Colors.white),
+                calcButton('2', Colors.grey.shade800, Colors.white),
+                calcButton('1', Colors.grey.shade800, Colors.white),
+                calcButton('+', Colors.orangeAccent, Colors.white),
+              ],
+            ),
+            SizedBox(height: 10,),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                // These are the functioning buttons
+                calcButton('( )', Colors.grey.shade800, Colors.white),
+                calcButton('0', Colors.grey.shade800, Colors.white),
+                calcButton('.', Colors.grey.shade800, Colors.white),
+                calcButton('=', Colors.orangeAccent, Colors.white),
+              ],
+            ),
+            SizedBox(height: 10,),
+          ],
+        ),
+      ),
     );
   }
 }
